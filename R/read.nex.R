7565680addd4fecde64cff28ed01c3c1c84a499d
Chad Eliason <chadeliason@wireless-10-145-175-39.public.utexas.edu>
2015-10-22 14:17:06 -0500

    read.nex

--- R/read.nex.R
+++ R/read.nex.R
@@ -29,11 +29,33 @@ read.nex <- function(file, missing = '?', gap = '-') {
 	# taxlabels <- str_match(x[taxlabelsstart:taxlabelsend], '[\\t]*(\\w+)')[,2]
 	taxlabels <- str_match(x[taxlabelsstart:taxlabelsend], '[\\t]*(.+)')[,2]
 	taxlabels <- gsub('^ ', '', taxlabels)
-
+	# this makes it possible to read mesquite saved nexus files with taxa in a single line separated by spaces
+	if (length(taxlabels)!=ntax){
+		taxlabels <- strsplit(taxlabels, split="\\s")[[1]]
+	}
 	# extract data matrix
 	matstart <- grep('MATRIX$', x, ignore.case=TRUE) + 1
 	matend <- grep('\\;', x[matstart:length(x)])[1] + matstart - 2
-	mat <- str_replace_all(x[matstart:matend], taxlabels, "")  # remove taxon labels
+	# start edit
+	# mesquite saves spaces between polymorphic characters (annoying)
+	mat <- paste0(x[matstart:matend], collapse="")
+
+	# add word boundaries for taxa without 
+	if (is.na(any(str_locate(pattern="\\s", taxlabels)))) {
+		locs <- str_locate(mat, paste0("\\b", taxlabels, "\\b", sep=""))
+	} else {
+		locs <- str_locate(mat, taxlabels)
+	}
+
+	mat <- str_sub(mat, start=locs[,1], end=c(locs[2:nrow(locs),1]-1, str_length(mat)))
+
+	mat <- str_replace_all(mat, taxlabels, "")
+
+	mat <- gsub("\\s", "", mat)
+
+	# stop edit
+
+	# mat <- str_replace_all(x[matstart:matend], taxlabels, "")  # remove taxon labels
 	mat <- gsub(',', '', mat)  # remove commas in data matrix
 	mat <- str_extract_all(mat, '\\d{1}|[\\(\\[\\{]\\d{1,4}[\\)\\]\\}]|\\-|\\?|\\w')  # extract scorings
 	mat <- lapply(mat, gsub, pattern='\\{|\\[', replacement='\\(')
@@ -55,7 +77,7 @@ read.nex <- function(file, missing = '?', gap = '-') {
 	
 	# symbols <- paste(sort(unique(as.vector(mat))), collapse="")
 	symbols <- gsub('[\\?\\-]','',symbols)
-	
+
 	mat <- ifelse(mat==missing, NA, mat)
 
 	# mat <- as.data.frame(mat)  # Maybe remove this??? slows things down a bit
@@ -100,9 +122,9 @@ read.nex <- function(file, missing = '?', gap = '-') {
 		res$charpartition <- rep("''", ncol(mat))
 	}
 
-	# get character names and numbers
-	if (length(grep('CHARLABELS', x, ignore.case=TRUE)) > 0) {
-		charlabelsstart <- grep('CHARLABELS', x, ignore.case=TRUE) + 1
+		# get character names and numbers
+	if (length(grep('\\bCHARLABELS', x, ignore.case=TRUE)) > 0) {
+		charlabelsstart <- grep('\\bCHARLABELS', x, ignore.case=TRUE) + 1
 		charlabelsend <- grep('\\;$', x[charlabelsstart:length(x)])[1] + charlabelsstart - 2	
 		charnames <- na.omit(str_match(x[charlabelsstart:charlabelsend], '\\[(\\d{1,4}|[A-Z]+)[.]*\\]\\s(.+)'))
 		charnames <- gsub("^'|'$", "", charnames)
@@ -116,8 +138,8 @@ read.nex <- function(file, missing = '?', gap = '-') {
 	}
 
 	# get state labels
-	if (length(grep('STATELABELS', x, ignore.case=TRUE)) > 0) {
-		statelabelsstart <- grep('STATELABELS', x, ignore.case=TRUE) + 1
+	if (length(grep('\\bSTATELABELS', x, ignore.case=TRUE)) > 0) {
+		statelabelsstart <- grep('\\bSTATELABELS', x, ignore.case=TRUE) + 1
 		statelabelsend <- grep('\\;$', x[statelabelsstart:length(x)])[1] + statelabelsstart - 2
 		statelabels <- str_match(x[statelabelsstart:statelabelsend], '[\\t]*\\d+\\s*(.+)$')[,2]
 		# sometimes states are on multiple lines:
@@ -139,7 +161,26 @@ read.nex <- function(file, missing = '?', gap = '-') {
 		res$statelabels <- rep("''", ncol(mat))
 	}
 
-		# res$charset <- charset
+	if (length(grep('\\bCHARSTATELABELS', x, ignore.case=TRUE)) > 0) {
+		charlabelsstart <- grep('\\bCHARSTATELABELS', x, ignore.case=TRUE) + 1
+		charlabelsend <- grep('\\;', x[charlabelsstart:length(x)])[1] + charlabelsstart - 1
+		# charstatelabels <- str_match_all(x[charlabelsstart:charlabelsend], "(\\t|\\,)\\s(\\d{1,})\\s'(.*?)'(\\s\\/\\s)?(.*?)(?=\\;|(\\,\\s\\d))")
+		
+		charstatelabels <- str_match_all(x[charlabelsstart:charlabelsend],
+			"(\\t|\\,)\\s(\\d{1,})\\s'(.*?)('\\s\\/(.*?))?(?=\\;(\\s)?$|(\\,\\s\\d{1,}\\s'))")
+
+# charstatelabels[[1]][3, ]
+# charstatelabels[[1]][19, ]
+# charstatelabels[[1]][1119, ]
+# charstatelabels[[1]][2504, ]
+
+		charnums <- as.numeric(charstatelabels[[1]][,3])
+		charlabels <- charstatelabels[[1]][,4]
+		statelabels <- charstatelabels[[1]][,6]
+		res$charlabels <- charlabels
+		res$charnums <- charnums
+		res$statelabels <- gsub("^\\s|\\s$", "", statelabels)
+	}
 
 	class(res) <- c('nex', 'list')
 	
