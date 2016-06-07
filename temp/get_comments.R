e5de996f0900da32356d709d68fad5ae5f7d804f
Chad Eliason <chadeliason@wireless-10-145-70-192.public.utexas.edu>
2016-06-07 15:13:32 -0500

    changes to get_comments, adds

--- temp/get_comments.R
+++ temp/get_comments.R
@@ -1,14 +1,42 @@
 # work with markup comments on chars
 
-txt <- readLines("/Users/chadeliason/Dropbox/Chad/phenome/data/2015-09-02/modified/final_reorderedJAC+CME.txt")
+file <- "/Users/chadeliason/Dropbox/phenome dataset/data/2015-09-02/modified/final_reorderedJAC+CME.txt"
+
+txt <- readLines(file)
+
 txt <- paste0(txt, collapse="\n")
 
 # find actions, comments associated with characters
-tmp <- str_match_all(txt, regex("\n([A-Z\\?]+)(\\d+)\\.(.*?)\\{(.*?)\\}", multiline=TRUE, dotall=TRUE))
+
+tmp <- str_match_all(txt, regex("\n([A-Z\\?]+)(\\d+)", multiline=TRUE, dotall=TRUE))
+
+todo <- tmp[[1]][,2]
+charnum <- as.numeric(tmp[[1]][,3])
+
+todo
+
+charnum
+
+
+
+
+
+
+
+
+str_match_all(txt, regex("\n([A-Z\\?]+)(\\d+)\\..*?\\{(.*?)\\}", multiline=TRUE, dotall=TRUE))
+
+# tmp <- str_match_all(txt, regex("\\{(.*?)\\}", multiline=TRUE, dotall=TRUE))
 tmp <- tmp[[1]]
 todo <- tmp[!is.na(tmp[,2]),2] # todo (e.g., DD - duplicated, XX - cut, KKM - keep and modify...)
 charnum <- tmp[!is.na(tmp[,3]),3] # charnum
 comments <- tmp[!is.na(tmp[,3]),5] # comments
+
+todo
+
+charnum
+
+comments
 
 # charnum
 # todo
