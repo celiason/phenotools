00ebce4b97c770bebb0301425225c368eb8143c0
Chad Eliason <chadeliason@wireless-10-145-33-222.public.utexas.edu>
2016-09-28 10:50:56 -0500

    duplicated
    made changes so first terms in character names are weighted more
    heavily, present/absent character states are excluded from considering
    of text distance, positional terms eliminated

--- R/duplicated.nex.R
+++ R/duplicated.nex.R
@@ -22,13 +22,10 @@
 #'
 #' @author Chad Eliason \email{chad_eliason@@utexas.edu}
 #'
-#' should have this output the string distances as well in the dup data frame @done
-#' exclude most common words during matching @todo
-#' exclude positional terms during matching @todo
-#' exclude common words during matching @todo
-#'
 
+# @done should have this output the string distances as well in the dup data frame
 
+# @done exclude positional terms during matching
 
 # Testing zone:
 # x <- read.nex("/Users/chadeliason/Documents/UT/projects/phenome/data/clarke_2002.nex")
@@ -82,33 +79,68 @@ if (!is.null(map)) {
 if (is.null(map)) {
   oldcharnames <- x$charlabels
   oldstatenames <- x$statelabels
-  newstatenames <- oldstatenames
-  newcharnames <- oldcharnames
+
+  # @done break apart character names into chunks (before comma, after comma)
+  matches <- str_match(oldcharnames, "^((.*),)?(.+)")
+  part1 <- matches[, 3]
+  part2 <- matches[, 4]
+  id <- is.na(part1)
+  part1[id] <- part2[id]
+  part2[id] <- NA
+  part3 <- oldstatenames
+
+  # newstatenames <- oldstatenames
+  # newcharnames <- oldcharnames
   # remove short words
   tocut <- c("to", "the", "and", "an", "a", "or", "of")
   tocut <- paste0("\\b", tocut, "\\b", collapse="|")
-  newcharnames <- gsub(tocut, "", newcharnames)
+  # newcharnames <- gsub(tocut, "", newcharnames)
+  part1 <- gsub(tocut, "", part1)
+  part2 <- gsub(tocut, "", part2)
+  part3 <- gsub(tocut, "", part3)
+
   # remove vague terms
   tocut <- c("form", "process", "state", "view", "margin", "shape", "placed")
   tocut <- paste0("\\b", tocut, "\\b", collapse="|")
-  newcharnames <- gsub(tocut, "", newcharnames)
+  # newcharnames <- gsub(tocut, "", newcharnames)
+  part1 <- gsub(tocut, "", part1)
+  part2 <- gsub(tocut, "", part2)
+  part3 <- gsub(tocut, "", part3)
+
   # remove positional terms
   tocut <- c("lateral", "distal", "ventral", "posterior", "anterior", "medial", "dors", "external")
   tocut <- paste0("\\b", tocut, "(\\w*)?", collapse="|")
-  newcharnames <- gsub(tocut, "", newcharnames)
-  
-  # @todo downweight based on commonness of term in dataset
+  # newcharnames <- gsub(tocut, "", newcharnames)
+  part1 <- gsub(tocut, "", part1)
+  part2 <- gsub(tocut, "", part2)
+  part3 <- gsub(tocut, "", part3)
   
   # clean up character names:
-  newstatenames <- cleantext(newstatenames)
-  newcharnames <- cleantext(newcharnames)
-  # @todo if state labels are absent/present (i.e. non-informative), then remove from duplicate matching
-  # @todo Weight first few words (if comma) more heavily
+  # newstatenames <- cleantext(newstatenames)
+  # newcharnames <- cleantext(newcharnames)
+  part1 <- cleantext(part1)
+  part2 <- cleantext(part2)
+  part3 <- cleantext(part3)
+
+  # Setup weightings
+
+  weighting <- cbind(rep(1, length(part1)), rep(1, length(part2)), rep(1, length(part3)))
+
+  # @done Weight first few words (if comma) more heavily
   # e.g., Dorsal vertebrae, shape -- vs Caudal vertebrae, shape (shouldn't be identified as duplicates)
-  # str_match(x$charlab, "^(.*?),")
+  id <- !is.na(part2)
+  weighting[id, 2] <- 0.5
+  weighting[is.na(part2), 2] <- 0
+
+  # @todo downweight based on commonness of term in dataset
+
+
+  # @done if state labels are absent/present (i.e. non-informative), then remove from duplicate matching
+  id <- grep("('present'\\s*'absent')|(present\\s*absent)|('absent'\\s*'present')|(absent\\s*present)", newstatenames)
+  weighting[id, 3] <- 0
 
   # generate all possible pairs of character combinations
-  pairids <- combn(seq_along(newcharnames), m=2)
+  pairids <- combn(seq_along(part1), m=2)
   # only want comparisons BETWEEN datasets/character types, not within:
   file <- x$file
   if (within_dataset) {  
@@ -124,17 +156,32 @@ if (is.null(map)) {
   stringdists <- numeric(length = ncol(pairids))
   # create progress bar
   pb <- txtProgressBar(min = 0, max = ncol(pairids), style = 3)
+
   # calculate text distances (takes ~200 seconds for 2.2M comparisons):
   # @TODO: WORK ON OPTIMIZING THIS PART
   for (i in 1:ncol(pairids)) {
-    # Sys.sleep(0.1)
-    str1a <- newcharnames[pairids[1,i]]
-    str1b <- newstatenames[pairids[1,i]]
-    str2a <- newcharnames[pairids[2,i]]
-    str2b <- newstatenames[pairids[2,i]]
-    stringdist1 <- stringdist(str1a, str2a, method = method)
-    stringdist2 <- stringdist(str1b, str2b, method = method)
-    stringdists[i] <- stringdist1 + stringdist2
+    id1 <- pairids[1, i]
+    id2 <- pairids[2, i]
+    str11 <- part1[id1]
+    str12 <- part2[id1]
+    str13 <- part3[id1]
+    str21 <- part1[id2]
+    str22 <- part2[id2]
+    str23 <- part3[id2]
+    wt1 <- sum(weighting[c(id1, id2), 1])
+    wt2 <- sum(weighting[c(id1, id2), 2])
+    wt3 <- sum(weighting[c(id1, id2), 3])
+    sd1 <- (1/wt1) * stringdist(str11, str21, method=method)
+    sd2 <- (1/wt2) * stringdist(str12, str22, method=method)
+    sd3 <- (1/wt3) * ifelse(any(weighting[c(id1, id2), 3]==0), NA, stringdist(str13, str23, method=method))
+    stringdists[i] <- sum(sd1, sd2, sd3, na.rm=TRUE)
+    # str1a <- newcharnames[pairids[1,i]]
+    # str1b <- newstatenames[pairids[1,i]]
+    # str2a <- newcharnames[pairids[2,i]]
+    # str2b <- newstatenames[pairids[2,i]]
+    # stringdist1 <- stringdist(str1a, str2a, method = method)
+    # stringdist2 <- stringdist(str1b, str2b, method = method)
+    # stringdists[i] <- stringdist1 + stringdist2
     setTxtProgressBar(pb, i)  # update progress bar
   }
   close(pb)
@@ -142,6 +189,7 @@ if (is.null(map)) {
   sset.dist <- sort(stringdists)
   sset <- as.numeric(names(stringdists))
 }
+
 
 
 # Training to identify duplicates
@@ -194,7 +242,7 @@ if (!train) {
 
   # answer <- character(length = length(sset))
 
-  # TODO: maybe use a different string distance metric (e.g., 'vomer' AND 'vomer extending laterally' give dist = 0)
+  # @TODO: maybe use a different string distance metric (e.g., 'vomer' AND 'vomer extending laterally' give dist = 0)
 
   # loop over pairs, wait for user input confirming redundant traits
   # sset = integers corresponding to row of pairids as possible matches
@@ -276,3 +324,17 @@ res$dups <- data.frame("char1" = dups[1, ], "charnum1" = x$charnum[dups[1, ]],
   "char2" = dups[2, ], "charnum2" = x$charnum[dups[2, ]], stringdist = stringdists.output)
 return(res)
 }
+
+
+
+
+# Test dataset
+# devtools::load_all("~/github/nexustools")
+# x <- read.nex("/Users/chadeliason/Documents/UT/projects/phenome/data/clarke_2002.nex")
+# x.dup <- duplicated(x, cutoff=0.5, train=TRUE)
+# head(x.dup$dups)
+# tail(x.dup$dups)
+# x$charlab[c(26, 27)]
+# x$charlab[c(57, 59)]
+
+
